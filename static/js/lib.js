c588865b748c3ed4f49fd402658c375b2ba1bcd5
Sagar Tiwari <fctorial@gmail.com>
2021-02-11 07:51:57 +0530

    higlight

--- static/js/lib.js
+++ static/js/lib.js
@@ -1,7 +1,5 @@
 function make_element (str, base = "div") {
-  console.log(str)
   const p = document.createElement(base)
   p.innerHTML = str
-  console.log(p)
   return p.children[0]
 }
--- static/js/nav.js
+++ static/js/nav.js
@@ -7,6 +7,9 @@ fetch("/collections/nav.json")
   .then(res => Object.keys(res).forEach(k => {
     const item = document.createElement('a')
     item.setAttribute('href', res[k])
+    if (res[k] === window.location.pathname) {
+      item.classList.add("current")
+    }
     item.innerText = k
     ne.appendChild(item)
   }))
